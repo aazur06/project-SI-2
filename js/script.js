dcefbbf2bd10ad3942519ae70522319690df4894
aazur06 <aaazur.06@gmail.com>
2024-12-15 19:15:10 +0100

    Project commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+  document.querySelector('.login-form-container').classList.remove('active');
+}
+
+
+function initializeSwiper(selector) {
+  return new Swiper(selector, {
+    grabCursor: true,
+    centeredSlides: true,  
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+}
+
+initializeSwiper(".vehicles-slider");
+initializeSwiper(".featured-slider");
+initializeSwiper(".review-slider");
